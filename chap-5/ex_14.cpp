3c135fa3e6d5c520aaedefbd28dd737bc6438ae6
Salil Mittal <salil.mittal05@gmail.com>
2021-03-22 13:48:02 +0530

    Modified chapter 5 exercise 14

--- chap-5/ex_14.cpp
+++ chap-5/ex_14.cpp
@@ -1,94 +1,100 @@
-// Chapter 05, exercise 14: read (day-of-the-week,value) pairs, collect values
-// for each day in a separate vector. Ignore illegal days, but accept common
-// synonyms. Print sum and list of values for each day in the end, count number
-// of rejected entries.
+/* Read (day-of-the-week,value) pairs from standard input. For
+example:
+Click here to view code image
+Tuesday 23 Friday 56 Tuesday –3 Thursday 99
+Collect all the values for each day of the week in a vector<int>.
+Write out the values of the seven day-of-the-week vectors. Print out
+the sum of the values in each vector. Ignore illegal days of the week,
+such as Funday, but accept common synonyms such as Mon and
+monday. Write out the number of rejected values. */
 
-#include "../std_lib_facilities.h"
+#include <iostream>
+#include <vector>
+#include <string>
 
-vector<int> mondays;
-vector<int> tuesdays;
-vector<int> wednesdays;
-vector<int> thursdays;
-vector<int> fridays;
-vector<int> saturdays;
-vector<int> sundays;
-int rejected = 0;
+std::vector<int> mondays;
+std::vector<int> tuesdays;
+std::vector<int> wednesdays;
+std::vector<int> thursdays;
+std::vector<int> fridays;
+std::vector<int> saturdays;
+std::vector<int> sundays;
 
-// put input into right vector, count rejected entries
-int handle_input(string day, int val)
-{
-    int cont = 0;
-    if (day=="Monday" || day=="monday" || day=="Mon" || day=="mon")
-        mondays.push_back(val);
-    else if (day=="Tuesday" || day=="tuesday" || day=="Tue" || day=="tue")
-        tuesdays.push_back(val);
-    else if (day=="Wednesday" || day=="wednesday" || day=="Wed" || day=="wed")
-        wednesdays.push_back(val);
-    else if (day=="Thursday" || day=="thursday" || day=="Thu" || day=="thu")
-        thursdays.push_back(val);
-    else if (day=="Friday" || day=="friday" || day=="Fri" || day=="fri")
-        fridays.push_back(val);
-    else if (day=="Saturday" || day=="saturday" || day=="Sat" || day=="sat")
-        saturdays.push_back(val);
-    else if (day=="Sunday" || day=="sunday" || day=="Sun" || day=="sun")
-        sundays.push_back(val);
-    else if (day=="q" && val==0)
-        cont = -1;
-    else ++rejected;
-    return cont;
-}
 
-// compute sum of vector elements
-int get_sum(vector<int> v)
-{
+void print_days() {
     int sum = 0;
-    for (int i = 0; i<v.size(); ++i) sum += v[i];
-    return sum;
-}
-
-// print weekday
-void print_day(string day, vector<int> v)
-{
-    cout << day << " (sum: " << get_sum(v) << ")\n";
-    cout << "Values:";
-    for (int i = 0; i<v.size(); ++i)
-        cout << ' ' << v[i];
-    cout << endl;
-}
-
-// print results: sum and values for each weekday, number of rejected entries
-void print_results()
-{
-    print_day("Monday",mondays);
-    print_day("Tuesday",tuesdays);
-    print_day("Wednesday",wednesdays);
-    print_day("Thursday",thursdays);
-    print_day("Friday",fridays);
-    print_day("Saturday",saturdays);
-    print_day("Sunday",sundays);
-    cout << rejected << " values rejected\n";
-}
-
-int main()
-try {
-    string day = " ";
-    int val = 0;
-    cout << "Enter weekday/value pairs, exit with 'q 0'\n";
-    while (cin>>day>>val) {
-        int cont = handle_input(day,val);
-        if (cont == -1) {
-            print_results();
-            return 0;
-        }
+    std::cout << "Monday: ";
+    for(int x : mondays) {
+        std::cout << x << " ";
+        sum+=x;
     }
-    error("Invalid input");
+    std::cout << "{" << sum << "}" << "\n";
+    sum = 0;
+
+    std::cout << "Tuesday: ";
+    for(int x : tuesdays) {
+        std::cout << x << " ";
+        sum+=x;
+    }
+    std::cout << "{" << sum << "}" << "\n";
+    sum = 0;
+
+    std::cout << "Wednesday: ";
+    for(int x : wednesdays) {
+        std::cout << x << " ";
+        sum+=x;
+    }
+    std::cout << "{" << sum << "}" << "\n";
+    sum = 0;
+
+    std::cout << "Thursday: ";
+    for(int x : thursdays) {
+        std::cout << x << " ";
+        sum+=x;
+    }
+    std::cout << "{" << sum << "}" << "\n";
+    sum = 0;
+    
+    std::cout << "Friday: ";
+    for(int x : fridays) {
+        std::cout << x << " ";
+        sum+=x;
+    }
+    std::cout << "{" << sum << "}" << "\n";
+    sum = 0;
+
+    std::cout << "Saturday: ";
+    for(int x : saturdays) {
+        std::cout << x << " ";
+        sum+=x;
+    }
+    std::cout << "{" << sum << "}" << "\n";
+    sum = 0;
+        std::cout << "Sunday: ";
+    for(int x : sundays) {
+        std::cout << x << " ";
+        sum+=x;
+    }
+    std::cout << "{" << sum << "}" << "\n";
+    sum = 0;
 }
-catch (exception& e) {
-    cerr << "Error: " << e.what() << endl;
-    //keep_window_open();
-    return 1;
-}
-catch (...) {
-    cerr << "Unknown exception!\n";
-    return 2;
+int main() {
+    std::string day;
+    int val;
+    int rejected;
+    std::cout << "Enter day value pairs: ";
+
+    for(; std::cin >> day >> val;) {
+        if(day == "Monday" || day == "monday" || day == "mon") mondays.push_back(val);
+        else if(day == "Tuesday" || day == "tuesday" || day == "tue") tuesdays.push_back(val);
+        else if(day == "Wednesday" || day == "wednesday" || day == "wed") wednesdays.push_back(val);
+        else if(day == "Thursday" || day == "thursday" || day == "thu") thursdays.push_back(val);
+        else if(day == "Friday" || day == "friday" || day == "fri") fridays.push_back(val);
+        else if(day == "Saturday" || day == "saturday" || day == "sat") saturdays.push_back(val);
+        else if(day == "Sunday" || day == "sunday" || day == "sun") sundays.push_back(val);
+        else rejected++;
+    }
+
+    print_days();
+    std::cout << "Rejected: " << rejected;
 }
